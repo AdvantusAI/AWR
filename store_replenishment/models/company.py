95a60228295c2e832d80a59f33f86b8d3054db8c
AdvantusAI <hugo@advantusai.com>
2025-05-03 00:26:43 -0600

    removed store replenishment

--- store_replenishment/models/company.py
+++
@@ -1,6 +0,0 @@
-# app/models/company.py
-class Company(Base):
-    __tablename__ = 'company'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False)
-    default_service_level = Column(Float, default=0.95)  # e.g., 0.95 for 95%
